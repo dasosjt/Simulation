9de44e716911b4a405afa3528487ab7ccef4383b
Diego Sosa <diego.stt@live.com>
2016-09-06 10:31:58 -0600

    hist

--- proyecto2/acumf.py
+++ proyecto2/acumf.py
@@ -12,26 +12,36 @@ def p1(x):
     return x*np.exp(x)
 
 def p2(x):
-    return 2*x*np.exp(x)
+    return -2*x*np.exp(x)
 
-def acump(pi, npi):
-    if(sum(npi) != 1):
-        print "Suma de "
-        
-    return 0
-
-
-#gaussian_numbers = np.random.randn(100)
-#plt.hist(gaussian_numbers)
-
-
-pi = [p1, p2]
-npi = [0.5, 0.5]
-
-uniform_rands = np.random.random(1000000)
-
-param = 1.8
-transformed_rands = [1 / param * np.log(1 / (1 - x)) for x in uniform_rands]
-plt.hist(transformed_rands, bins=500, normed=True)
-
-plt.show()
+def v(f, dist):
+    if(sum(dist) != 1):
+        print "Suma de probabilidades no es 1"
+    else: 
+        print "Suma de probabilidades es 1"
+        interval = []
+        for u in dist:
+            if len(interval) != 0:
+                interval.append(u+interval[-1])
+            else:
+                interval.append(u)
+        print interval
+        x = np.random.random(1000)
+        c = [0]*len(interval)
+        for xn in x:
+            for i in range(0,len(interval)):
+                if xn < interval[i] and xn > interval[i-1]:
+                    print "C "+str(i)+" +1 con el valor "+ str(xn)
+                    c[i]+=1
+                if i == 0 and xn < interval[0]:
+                    print "C "+str(i)+" +1 con el valor "+ str(xn)
+                    c[i]+=1
+        print c[0]
+        print c[1]
+        print c[2]
+        print c[3]
+        return c
+            
+f = [p1, p2]
+dist = [0.1, 0.3, 0.2, 0.4]
+plt.hist(v(f,dist), 500, normed=100, facecolor='g', alpha=0.75)
