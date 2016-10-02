ef38bd3770e375d2db342786995bfaf26a6be14b
Link-djst <diego.stt@live.com>
2016-10-01 22:47:12 -0600

    p4done

--- proyecto3/servers.py
+++ proyecto3/servers.py
@@ -21,7 +21,7 @@ C = []
 A = []
 D = []
 T = 2500
-servers = 29
+servers = 10
 lambdapp = 40
 a = 10
 
@@ -32,27 +32,31 @@ td = [inf]*servers
 ss = [0]*servers
 C = [0]*servers
 tx = [0]*servers
+cola = []
 
 
 while t < T or n >= 0:
-    print t
-    #print "A, ",A
-    #print "D, ",D
-    #print "C, ",C
+    print C
     if ta == min(ta, min(td)) and ta < T:
         print "Case 1"
         t = ta
         Na += 1
-        n += 1
         ta = t + exponencial(lambdapp)
+        t = ta
         A.append(t)
-        tempindex = findIndexOf(td, inf)
-        if tempindex != -1:
-            Y = exponencial(a)
-            td[tempindex] = t + Y
-            ss[tempindex] = Na
-            tx[tempindex] += Y
-            C[tempindex] += 1
+        if n  > len(td)-1:
+            n += 1
+            en_cola = exponencial(lambdapp)
+            cola.append(en_cola)
+        else:
+            tempindex = findIndexOf(td, inf)
+            if tempindex != -1:
+                Y = exponencial(a)
+                td[tempindex] = t + Y
+                ss[tempindex] = Na
+                tx[tempindex] += Y
+                C[tempindex] += 1
+
 
     elif min(td) < ta and min(td) < T:
         print "Case 2"
@@ -61,11 +65,15 @@ while t < T or n >= 0:
         n += -1
         D.append(t)
         tempindex = findIndexOf(td, t)
-        if n <= len(td):
+        if n <= len(td)-1:
+            if n == 1:
+                n = 0
+            else:
+                n = n - 1
             td[tempindex] = inf
             ss[tempindex] = 0
         else:
-            Na += 1
+            n += 1
             A.append(t)
             Y = exponencial(a)
             td[tempindex] = t + Y
@@ -74,15 +82,13 @@ while t < T or n >= 0:
             C[tempindex] += 1
 
     elif min(ta, min(td)) > T and n > 0:
-        print "Case 3"
         t = min(td)
         n += -1
         Nd += 1
         D.append(t)
         tempindex = findIndexOf(td, t)
         if n > 0:
-            Na += 1
-            A.append(t)
+            Nd += 1
             Y = exponencial(a)
             td[tempindex] = t + Y
             ss[tempindex] = Na
@@ -90,10 +96,47 @@ while t < T or n >= 0:
             C[tempindex] += 1
 
     elif n == 0:
-        print "Case 4"
+        #print "Case 4"
         break
-print sum(C)
-print C
-print sum(tx)
-print tx
-print n
+if len(cola) > 0:
+    pcola = sum(cola)/len(cola)
+else:
+    pcola = sum(cola)
+
+if sum(cola) > 0:
+    scola = len(cola)/sum(cola)
+else:
+    scola = len(cola)
+
+
+nQ = 0
+tQ = 0
+
+for i in range(0, len(tx)-1):
+    if(0 < D[i]-A[i+1]):
+        nQ += 1
+        tQ += (D[i]-A[i+1])
+tQ = sum(tx) - tQ
+
+print "En promedio cada servidor atendio ", sum(C)/servers
+
+for c0 in C:
+    print "El servidor atendio ", c0
+
+print "Los servidores estuvieron ocupados", sum(tx)
+
+for tx0 in tx:
+    print "El servidor estuvo ocupado ", tx0
+
+print "Los servidores estuvieron desocupados", sum(tx) - T
+
+for tx0 in tx:
+    print "El servidor estuvo desocupado ", T - tx0
+
+print "Cantidad de solicitudes que estuvieron en cola ",nQ
+
+print "En promedio cada solicitud estuvo en cola ", tQ/(nQ*servers*a)
+
+print "En promedio cada segundo estuvo en cola ", tQ/(T*servers*a)
+
+print "Ultima salida ocurrio", t
---
+++ proyecto4/totito3d.py
@@ -0,0 +1,204 @@
+import numpy as np
+import copy
+
+board = [
+  [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ],
+  [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ],
+  [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ],
+  [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]
+]
+
+
+p1_token = 1
+p2_token = -1
+draw_token = 0
+
+
+def slice_winner(state_slice):
+  slice_size = len(state_slice)
+  sums = [sum(row) for row in state_slice]
+  sums.extend([sum([row[i] for row in state_slice]) for i in range(slice_size)])
+
+  if (p1_token * slice_size) in sums:
+    return p1_token
+  elif (p2_token * slice_size) in sums:
+    return p2_token
+
+  return 0
+
+
+def winner(state):
+  for state_slice in state:
+    winner_in_slice = slice_winner(state_slice)
+    if winner_in_slice != draw_token:
+      return winner_in_slice
+
+  state_size = len(state)
+
+  for i in range(state_size):
+    state_slice = []
+    for j in range(state_size):
+      state_slice.append([state[j][i][k] for k in range(state_size)])
+
+    winner_in_slice = slice_winner(state_slice)
+
+    if winner_in_slice != draw_token:
+      return winner_in_slice
+
+  diagonals = [0, 0, 0, 0]
+  for i in range(state_size):
+    diagonals[0] += state[i][i][i]
+    diagonals[1] += state[state_size - 1 - i][i][i]
+    diagonals[2] += state[i][state_size - 1 - i][i]
+    diagonals[3] += state[state_size - 1 - i][state_size - 1 - i][i]
+
+  if (p1_token * state_size) in diagonals:
+    return p1_token
+
+  elif (p2_token * state_size) in diagonals:
+    return p2_token
+
+  return draw_token
+
+
+def str_token(cell):
+  if cell == p1_token:
+    return "X"
+  elif cell == p2_token:
+    return "O"
+
+  return "."
+
+def draw_board(state):
+  result = ""
+  state_size = len(state)
+  for y in range(state_size):
+    for z in range(state_size):
+      for x in range(state_size):
+        result += str_token(state[x][y][z]) + " "
+      result += "\t"
+    result += "\n"
+  return result
+
+
+player_1_turn = True
+occupied_position = []
+
+def monteCarloAI(board, occupied_position, n_simulation):
+    possible_position = {}
+    for i in range(n_simulation):
+        player_1_turn = False
+        first_turn = True
+        temp_occupied_position = copy.deepcopy(occupied_position)
+        temp_board = copy.deepcopy(board)
+
+        x = np.random.randint(4)
+        y = np.random.randint(4)
+        z = np.random.randint(4)
+        c = 128
+
+        temp_pos = [x, y, z]
+        temp_id = str(x)+"," + str(y) + "," + str(z)
+
+        while (temp_pos in temp_occupied_position):
+            x = np.random.randint(4)
+            y = np.random.randint(4)
+            z = np.random.randint(4)
+            temp_pos = [x, y, z]
+            temp_id = str(x)+"," + str(y) + "," + str(z)
+
+        #init temp_id as a possible possition
+        if not (temp_id in possible_position):
+            possible_position[temp_id] = 0
+
+        while winner(temp_board) == draw_token and c > 0:
+            #simulate a gameplay
+            if player_1_turn and not(first_turn):
+                [x, y, z] = np.random.randint(4, size = 3)
+            elif not(first_turn):
+                [x, y, z] = np.random.randint(4, size = 3)
+
+            if temp_board[x][y][z] == draw_token:
+                first_turn = False
+                temp_occupied_position.append([x,y,z])
+                temp_board[x][y][z] = 1 if player_1_turn else -1
+                player_1_turn = not player_1_turn
+            c += -1
+
+        if winner(temp_board) == 1:
+            possible_position[temp_id] += -1
+            #print "human sucks"
+        elif winner(temp_board) == -1:
+            possible_position[temp_id] += + 1
+            #print "MATRIX RULES"
+        else:
+            possible_position[temp_id] += 0
+            #print "tie... :("
+
+        #reinit temp_board and temp_occupied_position
+        temp_board = []
+        temp_occupied_position = []
+
+    #best chance to win
+    idmax =  max(possible_position, key=lambda k: possible_position[k])
+    result = map(int, idmax.split(','))
+    return result
+
+while winner(board) == draw_token:
+
+  # Print board state
+  print ""
+  print "Board:"
+  print draw_board(board)
+  print ""
+
+  # Print
+  print "Player %s turn:" % (1 if player_1_turn else 2)
+
+  # Get input for player 1
+  if player_1_turn:
+      x = int(raw_input("x: "))
+      y = int(raw_input("y: "))
+      z = int(raw_input("z: "))
+  else:
+      temp_board = copy.deepcopy(board)
+      [x, y, z] = monteCarloAI(temp_board, occupied_position, 10000)
+      #print "came back from monteCarloAI"
+
+  if board[x][y][z] == draw_token:
+    occupied_position.append([x,y,z])
+    board[x][y][z] = 1 if player_1_turn else -1
+    player_1_turn = not player_1_turn
+
+  else:
+    print ""
+    print "ERROR: occupied position, please retry in a new position"
+    print ""
+
+# Print board state
+print ""
+print "Board:"
+print draw_board(board)
+print ""
+
+print "Player %s is the winner!" % (1 if winner(board) == 1 else 2)
