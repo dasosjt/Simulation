31814dc1bd25f6d05e86ebeca3944e99aca84861
Link-djst <diego.stt@live.com>
2016-11-05 13:44:02 -0600

    changed tabs for spaces

--- proyecto_final/gui.py
+++ proyecto_final/gui.py
@@ -2,29 +2,29 @@ import pygame, math, random, sys
 from pygame.locals import *
 
 class Mosquitoe(pygame.sprite.Sprite):
-    "Returns: mosquitoe object"
-    "Function: update, new_pos"
-    "Atributes: vector"
+ "Returns: mosquitoe object"
+ "Function: update, new_pos"
+ "Atributes: vector"
 
-    def __init__(self, vector, position):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([10, 10])
-        self.image = self.image.convert()
-        self.image.fill((20,20,20))
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y  = position
-        self.vector = vector
+ def __init__(self, vector, position):
+  pygame.sprite.Sprite.__init__(self)
+  self.image = pygame.Surface([10, 10])
+  self.image = self.image.convert()
+  self.image.fill((20,20,20))
+  self.rect = self.image.get_rect()
+  self.rect.x, self.rect.y  = position
+  self.vector = vector
 
-    def update(self):
-        self.rect = self.new_pos(self.rect, self.vector)
+ def update(self):
+  self.rect = self.new_pos(self.rect, self.vector)
 
-    def new_pos(self, rect, vector):
-        (angle, z) = vector
-        (dx,dy) = (z*math.cos(angle), z*math.sin(angle))
-        return rect.move(dx,dy)
+ def new_pos(self, rect, vector):
+  (angle, z) = vector
+  (dx,dy) = (z*math.cos(angle), z*math.sin(angle))
+  return rect.move(dx,dy)
 
-    def draw(self, surface):
-        surface.blit(self.image, (self.rect.x, self.rect.y))
+ def draw(self, surface):
+  surface.blit(self.image, (self.rect.x, self.rect.y))
 
 
 "Screen"
@@ -43,17 +43,17 @@ m1 = Mosquitoe((0.86, 5), (30, 30))
 clock = pygame.time.Clock()
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                sys.exit()
+ for event in pygame.event.get():
+  if event.type == pygame.QUIT:
+   sys.exit()
+  elif event.type == KEYDOWN:
+   if event.key == K_ESCAPE:
+    sys.exit()
 
-    screen.blit(background, (0, 0))
-    m1.update()
-    m1.draw(screen)
+ screen.blit(background, (0, 0))
+ m1.update()
+ m1.draw(screen)
 
-    "60 fps"
-    clock.tick(60)
-    pygame.display.update()
+ "60 fps"
+ clock.tick(60)
+ pygame.display.update()
