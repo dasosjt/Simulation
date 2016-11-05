f47d1855c42d4547b4b3e07afc454950484421d6
Link-djst <diego.stt@live.com>
2016-11-05 13:03:06 -0600

    added first mosquitoe to scree

--- proyecto_final/gui.py
+++ proyecto_final/gui.py
@@ -4,21 +4,27 @@ from pygame.locals import *
 class Mosquitoe(pygame.sprite.Sprite):
     "Returns: mosquitoe object"
     "Function: update, new_pos"
-    "Atributes: area, vector"
+    "Atributes: vector"
 
-    def __init__(self, vector):
+    def __init__(self, vector, position):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_png('mosquitoe.png')
-        self.area = screen.get_rect()
+        self.image = pygame.Surface([10, 10])
+        self.image = self.image.convert()
+        self.image.fill((20,20,20))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y  = position
         self.vector = vector
 
 	def update(self):
-		self.rect = self.new_pos(self.rect,self.vector)
+		self.rect = self.new_pos(self.rect, self.vector)
 
 	def new_pos(self, rect, vector):
 		(angle,z) = vector
-		(dx,dy) = (z*math.cos(angle),z*math.sin(angle))
+		(dx,dy) = (z*math.cos(angle), z*math.sin(angle))
 		return rect.move(dx,dy)
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
 
 
 "Screen"
@@ -31,9 +37,7 @@ background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((255, 255, 255))
 
-"Blit everything to the screen"
-screen.blit(background, (0, 0))
-pygame.display.flip()
+m1 = Mosquitoe((0.3, 5), (30, 30))
 
 "Init clock"
 clock = pygame.time.Clock()
@@ -45,5 +49,10 @@ while True:
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 sys.exit()
+
+    screen.blit(background, (0, 0))
+    m1.draw(screen)
+
     "60 fps"
     clock.tick(60)
+    pygame.display.update()
