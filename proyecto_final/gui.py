27adca49194624b12a03e65d994e1742277890d3
Link-djst <diego.stt@live.com>
2016-11-12 13:55:18 -0600

    add distance and angle between in class mosquitoe

--- proyecto_final/gui.py
+++ proyecto_final/gui.py
@@ -1,4 +1,5 @@
 import pygame, math, random, sys
+import numpy as np
 from pygame.locals import *
 
 class Mosquitoe(pygame.sprite.Sprite):
@@ -14,6 +15,12 @@ class Mosquitoe(pygame.sprite.Sprite):
   self.rect = self.image.get_rect()
   self.rect.x, self.rect.y  = position
   self.vector = vector
+  "input layer"
+  self.input0 = 0
+  self.input1 = 0
+  self.input2 = 0
+  self.input3 = 0
+  self.input4 = 0
 
  def update(self):
   self.rect = self.new_pos(self.rect, self.vector)
@@ -21,12 +28,46 @@ class Mosquitoe(pygame.sprite.Sprite):
  def new_pos(self, rect, vector):
   "Vector with the direction and how much to move"
   (angle, z) = vector
-  (dx,dy) = (z*math.cos(angle), z*math.sin(angle))
+  #print np.rad2deg(angle)
+  (dx,dy) = (z*np.cos(angle), -z*np.sin(angle))
   return rect.move(dx,dy)
 
  def draw(self, surface):
   surface.blit(self.image, (self.rect.x, self.rect.y))
 
+ def angle_between(self, object_to):
+  dx = float(self.rect.centerx - object_to.rect.centerx)
+  dy = float(self.rect.centery - object_to.rect.centery)
+  rads = math.atan2(-dy,dx)
+  print np.rad2deg(rads)
+  return rads
+
+ def distance_between(self, object_to):
+  dx = float(self.rect.centerx - object_to.rect.centerx)
+  dy = float(self.rect.centery - object_to.rect.centery)
+  z = np.sqrt(np.power(dx,2) + np.power(dy,2))
+  return z
+
+ def distance_between_food(self, list_of_food):
+  "Reset Input Layer"
+  self.input0 = 0
+  self.input1 = 0
+  self.input2 = 0
+  self.input3 = 0
+  self.input4 = 0
+  for f in list_of_food:
+   rads = self.angle_between(f)
+   print np.rad2deg(rads)
+   if np.rad2deg(rads) < 15 and np.rad2deg(rads) > -15:
+    self.input2 = self.distance_between(f)
+   elif np.rad2deg(rads) < 35 and np.rad2deg(rads) > 15:
+    self.input1 = self.distance_between(f)
+   elif np.rad2deg(rads) < 45 and np.rad2deg(rads) > 35:
+    self.input0 = self.distance_between(f)
+   elif np.rad2deg(rads) < -15 and np.rad2deg(rads) > -35:
+    self.input3 = self.distance_between(f)
+   elif np.rad2deg(rads) < -35 and np.rad2deg(rads) > -45:
+    self.input4 = self.distance_between(f)
 
 "Screen"
 pygame.init()
@@ -38,7 +79,8 @@ background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((255, 255, 255))
 
-m1 = Mosquitoe((0.86, 5), (30, 30))
+m1 = Mosquitoe((0, 5), (0, 250))
+m2 = Mosquitoe((0.5*np.pi, 0), (250, 250))
 
 "Init clock"
 clock = pygame.time.Clock()
@@ -54,6 +96,9 @@ while True:
  screen.blit(background, (0, 0))
  m1.update()
  m1.draw(screen)
+ m2.update()
+ m2.draw(screen)
+ m2.angle_between(m1)
 
  "60 fps"
  clock.tick(60)
