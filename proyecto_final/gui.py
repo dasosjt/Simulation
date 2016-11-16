e8a94935a54266466b5ddf29931e859af07a4d5a
Link-djst <diego.stt@live.com>
2016-11-15 19:39:21 -0600

    Solo cambiar los inputs si la distancia es menor

--- proyecto_final/gui.py
+++ proyecto_final/gui.py
@@ -5,6 +5,7 @@ from pygame.locals import *
 
 WIDTH = 640
 HEIGHT = 480
+MAX_DISTANCE = math.sqrt(math.pow(WIDTH) + math.pow(HEIGHT))
 
 class Mosquitoe(pygame.sprite.Sprite):
  "Returns: mosquitoe object"
@@ -59,23 +60,25 @@ class Mosquitoe(pygame.sprite.Sprite):
 
  def distance_between_food(self, list_of_food):
   "Reset Input Layer"
-  self.input0 = 0
-  self.input1 = 0
-  self.input2 = 0
-  self.input3 = 0
-  self.input4 = 0
+  self.input0 = MAX_DISTANCE
+  self.input1 = MAX_DISTANCE
+  self.input2 = MAX_DISTANCE
+  self.input3 = MAX_DISTANCE
+  self.input4 = MAX_DISTANCE
   for f in list_of_food:
    rads = self.angle_between(f)
+   temp_distance = self.distance_between(f)
    print np.rad2deg(rads)
-   if np.rad2deg(rads) < 15 and np.rad2deg(rads) > -15:
+   print temp_distance
+   if np.rad2deg(rads) < 15 and np.rad2deg(rads) > -15 and self.input2 > temp_distance:
     self.input2 = self.distance_between(f)
-   elif np.rad2deg(rads) < 35 and np.rad2deg(rads) > 15:
+   elif np.rad2deg(rads) < 35 and np.rad2deg(rads) > 15 and self.input1 > temp_distance:
     self.input1 = self.distance_between(f)
-   elif np.rad2deg(rads) < 45 and np.rad2deg(rads) > 35:
+   elif np.rad2deg(rads) < 45 and np.rad2deg(rads) > 35 and self.input0 > temp_distance:
     self.input0 = self.distance_between(f)
-   elif np.rad2deg(rads) < -15 and np.rad2deg(rads) > -35:
+   elif np.rad2deg(rads) < -15 and np.rad2deg(rads) > -35 and self.input3 > temp_distance:
     self.input3 = self.distance_between(f)
-   elif np.rad2deg(rads) < -35 and np.rad2deg(rads) > -45:
+   elif np.rad2deg(rads) < -35 and np.rad2deg(rads) > -45 and self.input4 > temp_distance:
     self.input4 = self.distance_between(f)
 
 "Screen"
@@ -107,10 +110,8 @@ while True:
  screen.blit(background, (0, 0))
 
  for mosco in moscos:
-     mosco.update()
-     mosco.draw(screen)
-
- #moscos[1].angle_between(moscos[0])
+  mosco.update()
+  mosco.draw(screen)
 
  "60 fps"
  clock.tick(60)
